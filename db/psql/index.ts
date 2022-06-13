28879275c9614e83db7e0258c2bc848291c2a310
Andres Arango <ararango@comcast.net>
2022-06-13 15:30:53 -0700

    update route functions to parse data and handle erroneous products

--- db/psql/index.ts
+++ db/psql/index.ts
@@ -1,4 +1,4 @@
-import { Client, Pool } from "pg";
+import { Client, Pool } from 'pg';
 
 interface Info {
   id: number;
@@ -34,7 +34,7 @@ interface StyleResult {
   name: string;
   original_price: string;
   sale_price: string;
-  "default?": boolean;
+  'default?': boolean;
   photos: StylePhotos[];
   skus: SkuObj[];
 }
@@ -59,7 +59,7 @@ export async function getInfo(
     const offset = (page - 1) * count;
     const qValues = [count, offset];
     const infoRes = await client.query(
-      "SELECT * FROM info ORDER BY id LIMIT $1 OFFSET $2;",
+      'SELECT * FROM info ORDER BY id LIMIT $1 OFFSET $2;',
       qValues
     );
     await client.end();
@@ -82,29 +82,63 @@ export async function getProductDetails(
 
   try {
     await client.connect();
-    const detailsRes = await client.query("SELECT * FROM info WHERE id = $1;", [
-      productId,
-    ]);
-    if (detailsRes.rows.length === 0) {
-      await client.end();
-      return false;
-    }
-    const featuresRes = await client.query(
-      "SELECT feature, value FROM features WHERE product_id = $1;",
+    const detailsRes = await client.query(
+      `SELECT id, name, slogan, description, category, default_price,
+      (SELECT json_agg(
+        json_build_object(
+          'feature', feature,
+          'value', value
+        )
+      ) AS features FROM features WHERE product_id = info.id)
+        FROM info WHERE id = $1;`,
       [productId]
     );
     await client.end();
-    const productDetails = {
-      ...detailsRes.rows[0],
-      features: featuresRes.rows,
-    };
-    return productDetails;
+    if (detailsRes.rows.length === 0) {
+      return false;
+    }
+    return detailsRes.rows[0];
   } catch (e) {
     console.error(e);
     await client.end();
     return false;
   }
 }
+
+// export async function getProductDetails(
+//   productId: number
+// ): Promise<Details | false> {
+//   const client = new Client({
+//     database: process.env.DB_NAME,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PW,
+//   });
+
+//   try {
+//     await client.connect();
+//     const detailsRes = await client.query("SELECT * FROM info WHERE id = $1;", [
+//       productId,
+//     ]);
+//     if (detailsRes.rows.length === 0) {
+//       await client.end();
+//       return false;
+//     }
+//     const featuresRes = await client.query(
+//       "SELECT feature, value FROM features WHERE product_id = $1;",
+//       [productId]
+//     );
+//     await client.end();
+//     const productDetails = {
+//       ...detailsRes.rows[0],
+//       features: featuresRes.rows,
+//     };
+//     return productDetails;
+//   } catch (e) {
+//     console.error(e);
+//     await client.end();
+//     return false;
+//   }
+// }
 
 export async function getProductStyles(
   productId: number
@@ -118,58 +152,140 @@ export async function getProductStyles(
   try {
     await client.connect();
     const stylesRes = await client.query(
-      `SELECT * FROM styles WHERE productId = $1;`,
+      `SELECT json_agg(
+        json_build_object(
+          'style_id', id,
+          'name', name,
+          'original_price', original_price,
+          'sale_price', sale_price,
+          'default_style', default_style,
+          'photos', (
+            SELECT json_agg(
+              json_build_object(
+                'thumbnail_url', thumbnail_url,
+                'url', url
+              )
+            ) FROM photos where style_id = styles.id
+          ),
+          'skus', (
+            SELECT json_object_agg(
+              id::varchar(255), json_build_object(
+                'quantity', quantity,
+                'size', size
+              )
+            ) FROM skus where styleId = styles.id
+          )
+        )) AS results FROM styles WHERE productId = $1`,
       [productId]
     );
-    if (stylesRes.rows.length === 0) {
-      await client.end();
-      return false;
-    }
-    const styleIds = stylesRes.rows.map((row) => row.id);
-    const photosRes = await Promise.all(
-      styleIds.map(async (id) => {
-        let curRes = await client.query(
-          "SELECT url, thumbnail_url FROM photos WHERE style_id = $1;",
-          [id]
-        );
-        return curRes.rows;
-      })
-    );
-    const skusRes = await Promise.all(
-      styleIds.map(async (id) => {
-        let curRes = await client.query(
-          "SELECT size, quantity FROM skus WHERE styleId = $1;",
-          [id]
-        );
-        return curRes.rows;
-      })
-    );
-
     await client.end();
-    const results: StyleResult[] = [];
-    const productStyles = { product_id: productId.toString(), results };
-    styleIds.forEach((styleId, i) => {
-      const curStyle: StyleResult = {
-        style_id: Number(styleId),
-        name: stylesRes.rows[i].name,
-        original_price: stylesRes.rows[i].original_price,
-        sale_price:
-          stylesRes.rows[i].sale_price === null
-            ? "0"
-            : stylesRes.rows[i].sale_price,
-        "default?": stylesRes.rows[i].default_style,
-        photos: [...photosRes[i]],
-        skus: [...skusRes[i]],
-      };
-      productStyles.results.push(curStyle);
-    });
-    return productStyles;
+    if (stylesRes.rows[0].results === null) {
+      const results: StyleResult[] = [];
+      stylesRes.rows[0].results = results;
+    }
+    // remove null sale price
+    for (let i = 0; i < stylesRes.rows[0].results.length; i++) {
+      if (stylesRes.rows[0].results[i].sale_price === 'null') {
+        stylesRes.rows[0].results[i].sale_price = 0;
+      }
+
+      if (stylesRes.rows[0].results[i].photos === null) {
+        stylesRes.rows[0].results[i].photos = [
+          {
+            thumbnail_url: '',
+            url: '',
+          },
+        ];
+      }
+
+      if (stylesRes.rows[0].results[i].skus === null) {
+        stylesRes.rows[0].results[i].skus = [
+          {
+            skus: {
+              1: {
+                quantity: 0,
+                size: 'n/a',
+              },
+            },
+          },
+        ];
+      }
+    }
+
+    return {
+      product_id: productId.toString(),
+      ...stylesRes.rows[0],
+    };
   } catch (e) {
     console.error(e);
     await client.end();
   }
   return false;
 }
+// export async function getProductStyles(
+//   productId: number
+// ): Promise<Styles | false> {
+//   const client = new Client({
+//     database: process.env.DB_NAME,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PW,
+//   });
+
+//   try {
+//     await client.connect();
+//     const stylesRes = await client.query(
+//       `SELECT * FROM styles WHERE productId = $1;`,
+//       [productId]
+//     );
+//     if (stylesRes.rows.length === 0) {
+//       await client.end();
+//       return false;
+//     }
+//     const styleIds = stylesRes.rows.map((row) => row.id);
+//     const photosRes = await Promise.all(
+//       styleIds.map(async (id) => {
+//         let curRes = await client.query(
+//           'SELECT url, thumbnail_url FROM photos WHERE style_id = $1;',
+//           [id]
+//         );
+//         return curRes.rows;
+//       })
+//     );
+//     const skusRes = await Promise.all(
+//       styleIds.map(async (id) => {
+//         let curRes = await client.query(
+//           'SELECT size, quantity FROM skus WHERE styleId = $1;',
+//           [id]
+//         );
+//         return curRes.rows;
+//       })
+//     );
+
+//     await client.end();
+//     const results: StyleResult[] = [];
+//     const productStyles = { product_id: productId.toString(), results };
+//     styleIds.forEach((styleId, i) => {
+//       const curStyle: StyleResult = {
+//         style_id: Number(styleId),
+//         name: stylesRes.rows[i].name,
+//         original_price: stylesRes.rows[i].original_price,
+//         sale_price:
+//           stylesRes.rows[i].sale_price === null
+//             ? '0'
+//             : stylesRes.rows[i].sale_price,
+//         'default?': stylesRes.rows[i].default_style,
+//         photos: [...photosRes[i]],
+//         skus: [...skusRes[i]],
+//       };
+//       productStyles.results.push(curStyle);
+//     });
+//     return productStyles;
+//   } catch (e) {
+//     console.error(e);
+//     await client.end();
+//   }
+//   return false;
+// }
 
 export async function getRelated(productId: number): Promise<number[] | false> {
   const client = new Client({
@@ -181,7 +297,7 @@ export async function getRelated(productId: number): Promise<number[] | false> {
   try {
     await client.connect();
     const relatedRes = await client.query(
-      "SELECT related_product_id FROM related WHERE current_product_id = $1;",
+      'SELECT related_product_id FROM related WHERE current_product_id = $1;',
       [productId]
     );
 
