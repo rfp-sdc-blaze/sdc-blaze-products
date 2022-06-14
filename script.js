74b772f51f6affa3175383b737a587f590e87fbb
Andres Arango <ararango@comcast.net>
2022-06-13 22:27:59 -0700

    setup stress test functions for remaining routes

--- script.js
+++ script.js
@@ -5,10 +5,39 @@ export let options = {
   duration: '30s', //how long you want it to run
 };
 //Below randomize the endpoints
-export default function () {
-  http.get(
-    `http://localhost:3001/products/${
-      Math.floor(Math.random() * (1000011 - 1 + 1)) + 1
-    }`
-  );
-}
+// GET /products/:product_id
+// export default function () {
+//   http.get(
+//     `http://localhost:3001/products/${
+//       Math.floor(Math.random() * (1000011 - 1 + 1)) + 1
+//     }`
+//   );
+// }
+
+// GET /products
+// export default function () {
+//   http.get('http://localhost:3001/products/');
+// }
+
+// GET /products?count=900&page=1000
+// export default function () {
+//   http.get('http://localhost:3001/products?count=900&page=1000');
+// }
+
+// GET /products/:product_id/styles
+// export default function () {
+//   http.get(
+//     `http://localhost:3001/products/${
+//       Math.floor(Math.random() * (1000011 - 1 + 1)) + 1
+//     }/styles`
+//   );
+// }
+
+// GET /products/:product_id/related
+// export default function () {
+//   http.get(
+//     `http://localhost:3001/products/${
+//       Math.floor(Math.random() * (1000011 - 1 + 1)) + 1
+//     }/related`
+//   );
+// }
