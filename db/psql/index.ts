723a06acee3441229c78c887e0798cd93e3e452c
Andres Arango <ararango@comcast.net>
2022-06-14 16:57:07 -0700

    add port to pool

--- db/psql/index.ts
+++ db/psql/index.ts
@@ -5,6 +5,7 @@ export const db = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PW,
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
 });
 
 interface Info {
