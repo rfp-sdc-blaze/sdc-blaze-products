7c4ae9b8ad05fec7791f0f171115730ba3b88c1d
Andres Arango <ararango@comcast.net>
2022-06-13 21:05:33 -0700

    create k6 test config

---
+++ script.js
@@ -0,0 +1,14 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+export let options = {
+  vus: 1000, //stimulate how many virtual users
+  duration: '30s', //how long you want it to run
+};
+//Below randomize the endpoints
+export default function () {
+  http.get(
+    `http://localhost:3001/products/${
+      Math.floor(Math.random() * (1000011 - 1 + 1)) + 1
+    }`
+  );
+}
